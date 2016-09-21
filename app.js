aa4bd278d3344632f847dcccdcd710c0e2343690
shasidharjavvaji <javvajishasidhar@gmail.com>
2016-09-21 14:19:05 +0530

    Session 5 assignment 4

---
+++ app.js
@@ -0,0 +1,50 @@
+var employeeArray = [
+{name:"Shasidhar",
+		age:30,
+		Salary:500000,
+		Address : {
+             city:"Vijayawada",
+             state:"Andhra pradesh",
+             pincode:520010
+			 }
+},      
+{name:"Kartheek",
+		age:28,
+		Salary:450000,
+		Address : {
+             city:"Vijayawada",
+             state:"Andhra pradesh",
+             pincode:520010
+			 }
+},
+{name:"Yesaswini",
+		age:23,
+		Salary:60000,
+		Address : {
+             city:"Vijayawada",
+             state:"Andhra pradesh",
+             pincode:520010
+			 }
+},
+{name:"Chaturya",
+		age:24,
+		Salary:70000,
+		Address : {
+             city:"Vijayawada",
+             state:"Andhra pradesh",
+             pincode:520010
+			 }
+},
+{name:"Kavya",
+		age:27,
+		Salary:400000,
+		Address : {
+             city:"Vijayawada",
+             state:"Andhra pradesh",
+             pincode:520010
+			 }
+}	
+
+
+];
+console.log("Name:"+employeeArray[3].name+"\nAge:"+employeeArray[3].age+"\nSalary:"+employeeArray[3].Salary+"\nCity:"+employeeArray[3].Address.city+"\nstate:"+employeeArray[3].Address.state+"\npincode:"+employeeArray[3].Address.pincode);
